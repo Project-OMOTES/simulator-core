3bf43154998bb4727de655312777826b849f625c
mike van meerkerk <mvmeerkerk@outlook.com>
2023-10-09 16:02:23 +0200

    Added docstring and abstractmethod
    Added abstractmethod get and set, renamed register_pp.

--- src/simulator_core/entities/assets/asset_abstract.py
+++ src/simulator_core/entities/assets/asset_abstract.py
@@ -1,11 +1,28 @@
 from abc import ABC, abstractmethod
+from typing import Dict
+
+from pandas import DataFrame
 
 
 class AssetAbstract(ABC):
+    """Abstract class for Asset"""
+
     @abstractmethod
-    def register_pp(self):
+    def set(self, **kwargs) -> None:
+        """Placeholder to set attributes of an asset"""
         pass
 
     @abstractmethod
-    def get_timeseries(self):
+    def get(self, **kwargs) -> Dict:
+        """Placeholder to get attributes of an asset"""
+        pass
+
+    @abstractmethod
+    def register_pandapipes(self) -> None:
+        """Placeholder to register asset in a pandapipes network"""
+        pass
+
+    @abstractmethod
+    def get_timeseries(self) -> DataFrame:
+        """Get timeseries as a dataframe from a pandapipes asset"""
         pass
