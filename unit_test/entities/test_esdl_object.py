cd8bdc6d496923b66299552952670375ffc84138
samvanderzwan <sam.vanderzwan@deltares.nl>
2023-10-13 11:12:10 +0200

    Added test for esdl_asset_object

--- unit_test/entities/test_esdl_object.py
+++ unit_test/entities/test_esdl_object.py
@@ -1,7 +1,7 @@
-from simulator_core.entities.esdl_object import EsdlObject
+from simulator_core.entities.esdl_object import EsdlObject, EsdlAssetObject
 from simulator_core.infrastructure.utils import pyesdl_from_file
 import unittest
-
+from simulator_core.entities.assets import AssetAbstract, DemandCluster, ProductionCluster, Pipe
 
 class EsdlObjectTest(unittest.TestCase):
 
@@ -23,3 +23,20 @@ class EsdlObjectTest(unittest.TestCase):
         self.assertEqual(len(producers),1)
         self.assertEqual(len(consumers),1)
         self.assertEqual(len(pipes),2)
+
+    def test_EsdlAssetObject(self):
+        # Arrange
+        producer = 'producer'
+        consumer = 'consumer'
+        pipe = 'pipe'
+        producers = self.esdl_object.get_all_assets_of_type(producer)
+        consumers = self.esdl_object.get_all_assets_of_type(consumer)
+        pipes = self.esdl_object.get_all_assets_of_type(pipe)
+        # Act
+        asset_producer = producers[0].convert_esdl()
+        asset_consumer = consumers[0].convert_esdl()
+        asset_pipe = pipes[0].convert_esdl()
+        # Assert
+        self.assertTrue(isinstance(asset_producer, ProductionCluster))
+        self.assertTrue(isinstance(asset_consumer, DemandCluster))
+        self.assertTrue(isinstance(asset_pipe, Pipe))
