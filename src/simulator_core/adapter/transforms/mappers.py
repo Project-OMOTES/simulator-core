0b0f8d12cdeae3ab9d00296a37402429da9e207f
samvanderzwan <sam.vanderzwan@deltares.nl>
2023-11-06 10:39:25 +0100

    Addded class and testing for conversion between esdl class and atring

--- src/simulator_core/adapter/transforms/mappers.py
+++ src/simulator_core/adapter/transforms/mappers.py
@@ -1,4 +1,6 @@
 """Module with classes to convert esdl objects."""
+import esdl
+
 from simulator_core.simulation.mappers.mappers import EsdlMapperAbstract
 from simulator_core.entities.assets import AssetAbstract, ProductionCluster
 from simulator_core.entities import HeatNetwork, NetworkController, EsdlObject, EsdlAssetObject
@@ -56,3 +58,47 @@ class ProductionAssetMapper(EsdlAssetMapper):
 
     def to_entity(self, model: EsdlAssetObject) -> ProductionCluster:
         pass
+
+
+class StringEsdlAssetMapper(EsdlMapperAbstract):
+    """
+    Mapper class to convert strings to an esdl class type and vica-versa.
+    Please notate that the str_to_type_dict needs to have unique keys and values.
+    """
+    str_to_type_dict = {
+        'asset': esdl.Asset,
+        'producer': esdl.Producer,
+        'consumer': esdl.Consumer,
+        'geothermal': esdl.GeothermalSource,
+        'conversion': esdl.Conversion,
+        'pipe': esdl.Pipe,
+        'transport': esdl.Transport,
+        'junction': esdl.Joint
+    }
+
+    def to_esdl(self, entity: str) -> type:
+        """Method to convert a string to esdl class type
+
+        :param str entity: String to be converted to an esdl class type object.
+        :return: ESDL class described in the string.
+        """
+        if entity not in self.str_to_type_dict:
+            raise NotImplementedError(entity + " not implemented in StringESDLAssetMapper class")
+        return self.str_to_type_dict[entity]
+
+    def to_entity(self, entity: type) -> str:
+        """Method to convert esdl type class to a string.
+
+        The dict values are converted to a list to find the index in the list.
+        Then the list of keys is created and the value at the index found is returned.
+        :param type entity: ESDl object clas to be converted to a string
+        :return: str belonging to the entity.
+        """
+        try:
+            index = list(self.str_to_type_dict.values()).index(entity)
+        except ValueError:
+            raise NotImplementedError(str(entity) + " not implemented in StringESDLAssetMapper "
+                                                    "class")
+        return list(self.str_to_type_dict.keys())[index]
+
+
--- unit_test/entities/test_esdl_object.py
+++ unit_test/entities/test_esdl_object.py
@@ -1,7 +1,11 @@
+import esdl
+
 from simulator_core.entities.esdl_object import EsdlObject, EsdlAssetObject
+from simulator_core.adapter.transforms.mappers import StringEsdlAssetMapper
 from simulator_core.infrastructure.utils import pyesdl_from_file
 import unittest
 from simulator_core.entities.assets import AssetAbstract, DemandCluster, ProductionCluster, Pipe
+
 
 class EsdlObjectTest(unittest.TestCase):
 
@@ -20,9 +24,9 @@ class EsdlObjectTest(unittest.TestCase):
         pipes = self.esdl_object.get_all_assets_of_type(pipe)
 
         # Assert
-        self.assertEqual(len(producers),1)
-        self.assertEqual(len(consumers),1)
-        self.assertEqual(len(pipes),2)
+        self.assertEqual(len(producers), 1)
+        self.assertEqual(len(consumers), 1)
+        self.assertEqual(len(pipes), 2)
 
     def test_EsdlAssetObject(self):
         # Arrange
@@ -40,3 +44,77 @@ class EsdlObjectTest(unittest.TestCase):
         self.assertTrue(isinstance(asset_producer, ProductionCluster))
         self.assertTrue(isinstance(asset_consumer, DemandCluster))
         self.assertTrue(isinstance(asset_pipe, Pipe))
+
+
+class StringEsdlAssetMapperTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.asset = esdl.Asset
+        self.producer = esdl.Producer
+        self.consumer = esdl.Consumer
+        self.geothermal_source = esdl.GeothermalSource
+        self.conversion = esdl.Conversion
+        self.pipe = esdl.Pipe
+        self.transport = esdl.Transport
+        self.joint = esdl.Joint
+        self.asset_str = "asset"
+        self.producer_str = "producer"
+        self.consumer_str = "consumer"
+        self.geothermal_source_str = "geothermal"
+        self.conversion_str = "conversion"
+        self.pipe_str = "pipe"
+        self.transport_str = "transport"
+        self.joint_str = "junction"
+
+    def test_to_string(self):
+        # Arrange
+        # Act
+        asset_str = StringEsdlAssetMapper().to_entity(self.asset)
+        producer_str = StringEsdlAssetMapper().to_entity(self.producer)
+        consumer_str = StringEsdlAssetMapper().to_entity(self.consumer)
+        geothermal_source_str = StringEsdlAssetMapper().to_entity(self.geothermal_source)
+        conversion_str = StringEsdlAssetMapper().to_entity(self.conversion)
+        pipe_str = StringEsdlAssetMapper().to_entity(self.pipe)
+        transport_str = StringEsdlAssetMapper().to_entity(self.transport)
+        joint_str = StringEsdlAssetMapper().to_entity(self.joint)
+        # Assert
+        self.assertTrue(asset_str == self.asset_str)
+        self.assertTrue(producer_str == self.producer_str)
+        self.assertTrue(consumer_str == self.consumer_str)
+        self.assertTrue(geothermal_source_str == self.geothermal_source_str)
+        self.assertTrue(conversion_str == self.conversion_str)
+        self.assertTrue(pipe_str == self.pipe_str)
+        self.assertTrue(transport_str == self.transport_str)
+        self.assertTrue(joint_str == self.joint_str)
+
+    def test_to_esdl(self):
+        # Arrange
+        # Act
+        asset = StringEsdlAssetMapper().to_esdl(self.asset_str)
+        producer = StringEsdlAssetMapper().to_esdl(self.producer_str)
+        consumer = StringEsdlAssetMapper().to_esdl(self.consumer_str)
+        conversion = StringEsdlAssetMapper().to_esdl(self.conversion_str)
+        pipe = StringEsdlAssetMapper().to_esdl(self.pipe_str)
+        transport = StringEsdlAssetMapper().to_esdl(self.transport_str)
+        joint = StringEsdlAssetMapper().to_esdl(self.joint_str)
+        geothermal = StringEsdlAssetMapper().to_esdl(self.geothermal_source_str)
+        # Assert
+        self.assertTrue(asset == self.asset)
+        self.assertTrue(producer == self.producer)
+        self.assertTrue(consumer == self.consumer)
+        self.assertTrue(conversion == self.conversion)
+        self.assertTrue(pipe == self.pipe)
+        self.assertTrue(transport == self.transport)
+        self.assertTrue(joint == self.joint)
+        self.assertTrue(geothermal == self.geothermal_source)
+
+    def test_raise_error(self):
+        # Arrange
+        test_string = "nothing"
+        test_esdl_asset = esdl.WindTurbine
+        # Act
+        # Assert
+        with self.assertRaises(NotImplementedError):
+            StringEsdlAssetMapper().to_esdl(test_string)
+        with self.assertRaises(NotImplementedError):
+            StringEsdlAssetMapper().to_entity(test_esdl_asset)
