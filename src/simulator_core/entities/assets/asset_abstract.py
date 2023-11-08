3bed6279862d14d434454bf31ac21ec6b9c5a4af
mike van meerkerk <mvmeerkerk@outlook.com>
2023-11-08 16:12:42 +0100

    Resolve Flake8 D400 error

--- src/simulator_core/entities/assets/asset_abstract.py
+++ src/simulator_core/entities/assets/asset_abstract.py
@@ -5,24 +5,24 @@ from pandas import DataFrame
 
 
 class AssetAbstract(ABC):
-    """Abstract class for Asset"""
+    """Abstract class for Asset."""
 
     @abstractmethod
     def set(self, **kwargs) -> None:
-        """Placeholder to set attributes of an asset"""
+        """Placeholder to set attributes of an asset."""
         pass
 
     @abstractmethod
     def get(self, **kwargs) -> Any:
-        """Placeholder to get attributes of an asset"""
+        """Placeholder to get attributes of an asset."""
         pass
 
     @abstractmethod
     def register_pandapipes(self, **kwargs) -> None:
-        """Placeholder to register asset in a pandapipes network"""
+        """Placeholder to register asset in a pandapipes network."""
         pass
 
     @abstractmethod
     def get_timeseries(self, **kwargs) -> DataFrame:
-        """Get timeseries as a dataframe from a pandapipes asset"""
+        """Get timeseries as a dataframe from a pandapipes asset."""
         pass
