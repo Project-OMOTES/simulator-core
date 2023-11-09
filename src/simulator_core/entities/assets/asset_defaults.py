033d3c96b2689fadee64e41867fd9711ff0af207
mike van meerkerk <mvmeerkerk@outlook.com>
2023-11-09 16:16:47 +0100

    Additional parameter names and format changes

--- src/simulator_core/entities/assets/asset_defaults.py
+++ src/simulator_core/entities/assets/asset_defaults.py
@@ -1,4 +1,4 @@
-""" Define default values and names for assets. """
+"""Define default values and names for assets."""
 
 # Default values
 DEFAULT_DIAMETER = 1.2  # [m]
@@ -11,3 +11,6 @@ DEFAULT_NODE_HEIGHT = 0.0  # [m]
 PROPERTY_HEAT_DEMAND = "heat_demand"
 PROPERTY_TEMPERATURE_SUPPLY = "temperature_supply"
 PROPERTY_TEMPERATURE_RETURN = "temperature_return"
+PROPERTY_PRESSURE_SUPPLY = "pressure_supply"
+PROPERTY_PRESSURE_RETURN = "pressure_return"
+PROPERTY_MASSFLOW = "mass_flow"
