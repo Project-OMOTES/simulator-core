4c1c95ceaa2e2cad0ac141ee7a5c785dfb75d7ee
mike van meerkerk <mvmeerkerk@outlook.com>
2023-11-09 16:16:56 +0100

    Format changes

--- src/simulator_core/entities/assets/utils.py
+++ src/simulator_core/entities/assets/utils.py
@@ -1,3 +1,4 @@
+"""Utililty functions for assets."""
 from pandapipes import pandapipesNet
 
 
@@ -18,15 +19,30 @@ def heat_demand_and_temperature_to_mass_flow(
         "from_junction". The temperature should be supplied in Kelvin.
     :param pandapipesNet net: The pandapipes network used to calculate the specific heat capacity.
     """
-    if temperature_supply > temperature_return:
-        return thermal_demand / (
-            (temperature_supply - temperature_return)
-            * float(pandapipes_net.fluid.get_heat_capacity(temperature_supply))
-        )
-    else:
-        return thermal_demand / (
-                    (temperature_return - temperature_supply)
-                    * float(pandapipes_net.fluid.get_heat_capacity(temperature_return))
-                )
+    heat_capacity = pandapipes_net.fluid.get_heat_capacity(
+        (temperature_return + temperature_supply) / 2
+    )
+    return thermal_demand / ((temperature_supply - temperature_return) * float(heat_capacity))
 
-    
+
+def mass_flow_and_temperature_to_heat_demand(
+    temperature_supply: float,
+    temperature_return: float,
+    mass_flow: float,
+    pandapipes_net: pandapipesNet,
+) -> float:
+    """Calculate the thermal demand that is met by the mass flow rate.
+
+    :param float temperature_supply: The temperature that the asset delivers to the "to_junction".
+        The temperature should be supplied in Kelvin. The supply temperature is used to calculate
+        the specific heat capacity of the fluid.
+    :param float temeprature_return: The temperature that the asset receives from the
+        "from_junction". The temperature should be supplied in Kelvin.
+    :param float mass_flow: The mass flow rate that is used to meet the thermal demand. The mass
+        flow rate should be supplied in kg/s.
+    :param pandapipesNet net: The pandapipes network used to calculate the specific heat capacity.
+    """
+    heat_capacity = pandapipes_net.fluid.get_heat_capacity(
+        (temperature_return + temperature_supply) / 2
+    )
+    return mass_flow * (temperature_supply - temperature_return) * float(heat_capacity)
