34eca481d9ac9ddcefd37ac71ef47434cb52381e
michieltukker <michiel.tukker@deltares.nl>
2023-11-09 13:40:04 +0100

    chore(all): Added copyright notice and docstrings

--- src/simulator_core/simulation/networksimulation.py
+++ src/simulator_core/simulation/networksimulation.py
@@ -1,11 +1,33 @@
+#  Copyright (c) 2023. Deltares & TNO
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+""" Simulates an heat network for the specified duration."""
+
 from simulator_core.entities import HeatNetwork, NetworkController, SimulationConfiguration
 
 
 class NetworkSimulation:
+    """NetworkSimulation connects the controller and HeatNetwork (incl. assets). """
     def __init__(self, network: HeatNetwork, controller: NetworkController):
+        """Instantiate the NetworkSimulation object"""
         self.config = None
         self.network = network
         self.controller = controller
 
     def run(self, config: SimulationConfiguration):
+        """Run the simulation.
+
+        :param SimulationConfiguration"""
         self.config = config
--- unit_test/entities/test_entities.py
+++ unit_test/entities/test_entities.py
@@ -1,10 +1,28 @@
+#  Copyright (c) 2023. Deltares & TNO
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import simulator_core.entities.heat_network
 from simulator_core.entities import HeatNetwork
 import unittest
 
 
 class HeatNetworkTest(unittest.TestCase):
+    """Testcase for HeatNetwork class."""
+
     def test_heat_network(self) -> None:
+        """Generic/template test for Heatnetwork."""
         # Arrange
 
         # Act
