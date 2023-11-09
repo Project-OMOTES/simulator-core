cf076a2893f6fa902e013e231b6f044928de3786
mike van meerkerk <mvmeerkerk@outlook.com>
2023-11-09 14:54:18 +0100

    Default names and variables

---
+++ src/simulator_core/entities/assets/asset_defaults.py
@@ -0,0 +1,13 @@
+""" Define default values and names for assets. """
+
+# Default values
+DEFAULT_DIAMETER = 1.2  # [m]
+DEFAULT_PRESSURE = 5.0  # [bar]
+DEFAULT_TEMPERATURE = 300.0  # [K]
+DEFAULT_TEMPERATURE_DIFFERENCE = 30.0  # [K]
+DEFAULT_NODE_HEIGHT = 0.0  # [m]
+
+# Default names
+PROPERTY_HEAT_DEMAND = "heat_demand"
+PROPERTY_TEMPERATURE_SUPPLY = "temperature_supply"
+PROPERTY_TEMPERATURE_RETURN = "temperature_return"
