779f1f1e367ef937f2f6eaa71de0bfd3b10500c4
samvanderzwan <sam.vanderzwan@deltares.nl>
2023-11-09 12:41:59 +0100

    moves string to esdl to seperate file

--- src/simulator_core/adapter/transforms/mappers.py
+++ src/simulator_core/adapter/transforms/mappers.py
@@ -2,8 +2,10 @@
 import esdl
 
 from simulator_core.simulation.mappers.mappers import EsdlMapperAbstract
-from simulator_core.entities.assets import AssetAbstract, ProductionCluster
-from simulator_core.entities import HeatNetwork, NetworkController, EsdlObject, EsdlAssetObject
+from simulator_core.entities.assets import AssetAbstract, ProductionCluster, EsdlAssetObject
+from simulator_core.entities.heat_network import HeatNetwork
+from simulator_core.entities.network_controller import NetworkController
+from simulator_core.entities.esdl_object import EsdlObject
 from typing import Any
 
 
@@ -60,44 +62,3 @@ class ProductionAssetMapper(EsdlAssetMapper):
         pass
 
 
-class StringEsdlAssetMapper(EsdlMapperAbstract):
-    """Mapper class to convert strings to an esdl class type and vica-versa.
-
-    Please note that the str_to_type_dict needs to have unique keys and values.
-    """
-
-    str_to_type_dict = {
-        'asset': esdl.Asset,
-        'producer': esdl.Producer,
-        'consumer': esdl.Consumer,
-        'geothermal': esdl.GeothermalSource,
-        'conversion': esdl.Conversion,
-        'pipe': esdl.Pipe,
-        'transport': esdl.Transport,
-        'junction': esdl.Joint
-    }
-
-    def to_esdl(self, entity: str) -> type:
-        """Method to convert a string to esdl class type.
-
-        :param str entity: String to be converted to an esdl class type object.
-        :return: ESDL class described in the string.
-        """
-        if entity not in self.str_to_type_dict:
-            raise NotImplementedError(entity + " not implemented in StringESDLAssetMapper class")
-        return self.str_to_type_dict[entity]
-
-    def to_entity(self, entity: type) -> str:
-        """Method to convert esdl type class to a string.
-
-        The dict values are converted to a list to find the index in the list.
-        Then the list of keys is created and the value at the index found is returned.
-        :param type entity: ESDl object clas to be converted to a string
-        :return: str belonging to the entity.
-        """
-        try:
-            index = list(self.str_to_type_dict.values()).index(entity)
-        except ValueError:
-            raise NotImplementedError(str(entity) + " not implemented in StringESDLAssetMapper "
-                                                    "class")
-        return list(self.str_to_type_dict.keys())[index]
