3679c766416efff88fc41adf2e8ab8b5846d06ae
samvanderzwan <sam.vanderzwan@deltares.nl>
2023-11-15 15:39:48 +0100

    Fixed linting issues

--- src/simulator_core/adapter/transforms/esdl_asset_mapper.py
+++ src/simulator_core/adapter/transforms/esdl_asset_mapper.py
@@ -1,3 +1,4 @@
+"""Module containing the Esdl to asset mapper class."""
 from simulator_core.entities import EsdlAssetObject
 from simulator_core.entities.assets import ProductionCluster, DemandCluster, Pipe, AssetAbstract
 from simulator_core.simulation.mappers.mappers import EsdlMapperAbstract
@@ -8,6 +9,7 @@ from typing import Any
 
 class EsdlAssetMapper(EsdlMapperAbstract):
     """Creates entity Asset objects based on a PyESDL EnergySystem assets."""
+
     conversion_dict = {
         esdl.Producer: ProductionCluster,
         esdl.GenericProducer: ProductionCluster,
@@ -17,13 +19,15 @@ class EsdlAssetMapper(EsdlMapperAbstract):
     }
 
     def to_esdl(self, entity: AssetAbstract) -> Any:
-        """ Maps entity object to PyEsdl objects"""
+        """Maps entity object to PyEsdl objects."""
         raise NotImplementedError("EsdlAssetMapper.to_esdl()")
 
     def to_entity(self, model: EsdlAssetObject, **kwargs) -> AssetAbstract:
         """Method to map an esdl asset to an asset entity class.
+
         :param EsdlAssetObject model: Object to be converted to an asset entity.
-        :return: Entity object. """
+        :return: Entity object.
+        """
         if not type(model.esdl_asset) in self.conversion_dict:
             raise NotImplementedError(str(model.esdl_asset) + ' not implemented in conversion')
         return self.conversion_dict[type(model.esdl_asset)](model.esdl_asset.name,
