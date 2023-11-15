dc053d8544a28de2b7109af61ba44f295c7e8f8e
samvanderzwan <sam.vanderzwan@deltares.nl>
2023-11-15 13:42:34 +0100

    Added required method in demand cluster and pipe class

--- src/simulator_core/entities/assets/demand_cluster.py
+++ src/simulator_core/entities/assets/demand_cluster.py
@@ -16,14 +16,51 @@
 """ Demand cluster asset."""
 
 from .asset_abstract import AssetAbstract
-
+from typing import Dict
+from pandas import DataFrame
 
 class DemandCluster(AssetAbstract):
     def __init__(self):
         pass
 
-    def register_pp(self):
+    def set_setpoints(self, setpoints: Dict, **kwargs) -> None:
+        """Placeholder to set the setpoints of an asset prior to a simulation.
+
+        :param Dict setpoints: The setpoints that should be set for the asset.
+            The keys of the dictionary are the names of the setpoints and the values are the values
+        """
         pass
 
-    def get_timeseries(self):
+    def get_setpoints(self, **kwargs) -> Dict:
+        """Placeholder to get the setpoint attributes of an asset.
+
+        :return Dict: The setpoints of the asset. The keys of the dictionary are the names of the
+            setpoints and the values are the values.
+        """
+        pass
+
+    def simulation_performed(self) -> bool:
+        """Placeholder to indicate that a simulation has been performed.
+
+        :return bool: True if a simulation has been performed, False otherwise.
+        """
+        pass
+
+    def _create(self) -> None:
+        """Placeholder to create an asset in a pandapipes network."""
+        pass
+
+    def write_to_output(self) -> None:
+        """Placeholder to write the asset to the output.
+
+        The output list is a list of dictionaries, where each dictionary
+        represents the output of its asset for a specific timestep.
+        """
+        pass
+
+    def get_timeseries(self) -> DataFrame:
+        """Get timeseries as a dataframe from a pandapipes asset.
+
+        The header is a tuple of the asset id and the property name.
+        """
         pass
--- src/simulator_core/entities/assets/pipe.py
+++ src/simulator_core/entities/assets/pipe.py
@@ -1,12 +1,66 @@
+#  Copyright (c) 2023. Deltares & TNO
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from .asset_abstract import AssetAbstract
+from typing import Dict
+from pandas import DataFrame
 
 
 class Pipe(AssetAbstract):
     def __init__(self):
         pass
 
-    def register_pp(self):
+    def set_setpoints(self, setpoints: Dict, **kwargs) -> None:
+        """Placeholder to set the setpoints of an asset prior to a simulation.
+
+        :param Dict setpoints: The setpoints that should be set for the asset.
+            The keys of the dictionary are the names of the setpoints and the values are the values
+        """
         pass
 
-    def get_timeseries(self):
+    def get_setpoints(self, **kwargs) -> Dict:
+        """Placeholder to get the setpoint attributes of an asset.
+
+        :return Dict: The setpoints of the asset. The keys of the dictionary are the names of the
+            setpoints and the values are the values.
+        """
+        pass
+
+    def simulation_performed(self) -> bool:
+        """Placeholder to indicate that a simulation has been performed.
+
+        :return bool: True if a simulation has been performed, False otherwise.
+        """
+        pass
+
+    def _create(self) -> None:
+        """Placeholder to create an asset in a pandapipes network."""
+        pass
+
+    def write_to_output(self) -> None:
+        """Placeholder to write the asset to the output.
+
+        The output list is a list of dictionaries, where each dictionary
+        represents the output of its asset for a specific timestep.
+        """
+        pass
+
+    def get_timeseries(self) -> DataFrame:
+        """Get timeseries as a dataframe from a pandapipes asset.
+
+        The header is a tuple of the asset id and the property name.
+        """
         pass
