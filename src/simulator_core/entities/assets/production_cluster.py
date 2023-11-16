754ce9cb6bc5347fd7a5c3c90b1186434c471c26
samvanderzwan <sam.vanderzwan@deltares.nl>
2023-11-16 10:41:55 +0100

    Adjusted production cluster to work witht he new workflow not fully implemented

--- src/simulator_core/entities/assets/production_cluster.py
+++ src/simulator_core/entities/assets/production_cluster.py
@@ -18,7 +18,7 @@ from typing import Dict
 from warnings import warn
 
 import numpy as np
-from pandapipes import pandapipesNet
+
 from pandas import DataFrame
 
 from simulator_core.entities.assets.asset_abstract import AssetAbstract
@@ -49,79 +49,28 @@ class ProductionCluster(AssetAbstract):
     def __init__(
         self,
         asset_name: str,
-        asset_id: str,
-        from_junction: Junction,
-        to_junction: Junction,
-        thermal_production_required: float,
-        temperature_supply: float,
-        temperature_return: float = np.NaN,
-        height_m: float = DEFAULT_NODE_HEIGHT,
-        internal_diameter: float = DEFAULT_DIAMETER,
-        pressure_supply: float = DEFAULT_PRESSURE,
-        control_mass_flow: bool = False,
-    ):
+        asset_id: str):
         """Initialize a ProductionCluster object.
 
-        :param pandapipesNet pandapipes_net: The pandapipes network in which the
-            asset is created.
         :param str asset_name: The name of the asset.
         :param str asset_id: The unique identifier of the asset.
-        :param Junction from_junction: The junction where the asset starts.
-        :param Junction to_junction: The junction where the asset ends.
-        :param float thermal_production_required: The thermal allocation of the asset.
-            The thermal allocation should be supplied in Watts.
-        :param float temperature_supply: The temperature that the asset
-            delivers to the "to_junction". The temperature should be
-            supplied in Kelvin.
-        :param float temeprature_return: The temperature that the asset
-            receives from the "from_junction". The temperature should be
-            supplied in Kelvin. If the value is not supplied it defaults to
-            "temperature_supply" - DEFAULT_TEMPERATURE_DIFFERENCE.
-        :param float height_m: The height of the junctions. The height should
-            be supplied in meters, it defaults to DEFAULT_NODE_HEIGHT.
-        :param float internal_diameter: The internal diameter of the pipe.
-            The internal diameter should be supplied in meters, it defaults to
-            DEFAULT_DIAMETER.
-        :param float pressure_supply: The pressure that the asset delivers
-            to the "to_junction". The pressure should be supplied in bar, it
-            defaults to DEFAULT_PRESSURE.
-        :param bool control_mass_flow: If True, the mass flow rate of the asset
-            is controlled by the "Control Valve". If False, the mass flow rate
-            of the asset is floating.
         """
-        # Define the pandapipes network
-        self.pandapipes_net = None
-        # Define the asset properties
-        self.asset_name = asset_name
-        self.asset_id = asset_id
-        # TODO: Do we need to carry the junction ids?
-        self.from_junction = from_junction
-        self.to_junction = to_junction
-        self.internal_diameter = internal_diameter
-        self.height_m = height_m
+        super().__init__(asset_name, asset_id)
+        self.height_m = None
         # DemandCluster thermal and mass flow specifications
-        self.thermal_production_required = thermal_production_required
-        self.temperature_supply = temperature_supply
-        self.temperature_return = (
-            temperature_supply - DEFAULT_TEMPERATURE_DIFFERENCE
-            if np.isnan(temperature_return)
-            else temperature_return
-        )
+        self.thermal_production_required = None
+        self.temperature_supply = None
+        self.temperature_return = None
         # DemandCluster pressure specifications
-        self.pressure_supply = pressure_supply
-        self.control_mass_flow = control_mass_flow
-#        self._controlled_mass_flow = heat_demand_and_temperature_to_mass_flow(
-#            thermal_demand=self.thermal_production_required,
-#            temperature_supply=self.temperature_supply,
-#            temperature_return=self.temperature_return,
-#            pandapipes_net=self.pandapipes_net,
-#        )
+        self.pressure_supply = None
+        self.control_mass_flow = None
         # Objects of the asset
         self._initialized = False
-        #self._create()
-
         # Output list
         self.output = []
+
+    def add_physical_data(self, data: Dict[str, float]):
+        pass
 
     def _create(self) -> None:
         """Create a representation of the asset in pandapipes.
@@ -135,7 +84,6 @@ class ProductionCluster(AssetAbstract):
         - An intermediate junction to link both components.
         """
         if not self._initialized:
-            self._initialized = True
             # Create intermediate junction
             self._intermediate_junction = Junction(
                 pandapipes_net=self.pandapipes_net,
@@ -166,6 +114,7 @@ class ProductionCluster(AssetAbstract):
                 in_service=True,
                 name=f"flow_control_{self.asset_name}",
             )
+            self._initialized = True
 
     def _set_supply_temperature(self, temperature_supply: float) -> None:
         """Set the supply temperature of the asset.
