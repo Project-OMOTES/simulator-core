578ba1e548b7fc98ffcf81de2cf2b6b87e9817c2
samvanderzwan <sam.vanderzwan@deltares.nl>
2024-04-12 13:20:30 +0200

    changed the theme

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ author = 'Michiel Tukker, Sam van der Zwan, Ryvo Octaviano, Mike van Meerkerk'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['python_docs_theme'
+extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,7 +44,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
