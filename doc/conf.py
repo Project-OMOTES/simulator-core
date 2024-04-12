0a3460a0be05369ac14236a72dd13b4ee49dc909
samvanderzwan <sam.vanderzwan@deltares.nl>
2024-04-12 12:31:43 +0200

    Added correct themes to conf.py and correct small typo

--- doc/conf.py
+++ doc/conf.py
@@ -27,7 +27,7 @@ author = 'Michiel Tukker'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['python_docs_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
