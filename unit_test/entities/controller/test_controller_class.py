3109ebf7dfda919fa6b61dfd2d12a2b9221c076d
samvanderzwan <sam.vanderzwan@deltares.nl>
2024-07-09 17:07:18 +0200

    Added unit test for the network controller class

--- unit_test/entities/controller/test_controller_class.py
+++ unit_test/entities/controller/test_controller_class.py
@@ -14,16 +14,131 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """Test controller class."""
 import unittest
-
-import pandas as pd
+from unittest.mock import Mock
 from datetime import datetime
 
 from simulator_core.entities.assets.controller.controller_consumer import ControllerConsumer
 from simulator_core.entities.assets.controller.controller_producer import ControllerProducer
+from simulator_core.entities.network_controller import NetworkController
+from simulator_core.entities.assets.asset_defaults import (PROPERTY_HEAT_DEMAND,
+                                                           PROPERTY_TEMPERATURE_RETURN,
+                                                           PROPERTY_TEMPERATURE_SUPPLY,
+                                                           PROPERTY_SET_PRESSURE)
 
 
 class ControllerTest(unittest.TestCase):
     """Testcase for Controller class."""
 
+    def setUp(self):
+        """Set up the test case."""
+        self.producer1 = Mock()
+        self.producer1.id = "producer1"
+        self.producer1.power = 1.0
+        self.producer1.priority = 1
+        self.producer1.temperature_return = 20.0
+        self.producer1.temperature_supply = 30.0
+        self.producer2 = Mock()
+        self.producer2.id = "producer2"
+        self.producer2.power = 2.0
+        self.producer2.priority = 2
+        self.producer2.temperature_return = 40.0
+        self.producer2.temperature_supply = 50.0
+        self.consumer1 = Mock()
+        self.consumer1.get_heat_demand.return_value = 1.0
+        self.consumer1.temperature_return = 20.0
+        self.consumer1.temperature_supply = 30.0
+        self.consumer2 = Mock()
+        self.consumer2.get_heat_demand.return_value = 2.0
+        self.consumer2.temperature_return = 40.0
+        self.consumer2.temperature_supply = 50.0
+        self.controller = NetworkController(consumers=[self.consumer1, self.consumer2],
+                                            producers=[self.producer1, self.producer2])
 
+    def test_controller_init(self):
+        """Test to initialize the controller."""
+        consumer = ControllerConsumer("consumer", "id")
+        producer = ControllerProducer("producer", "id")
+        controller = NetworkController(consumers=[consumer], producers=[producer])
+        # Act
 
+        # Assert
+        self.assertEqual(controller.consumers, [consumer])
+        self.assertEqual(controller.producers, [producer])
+
+    def test_get_total_demand(self):
+        """Test to get the total demand of the network."""
+        # Act
+        total_demand = self.controller.get_total_demand(datetime.now())
+        # Assert
+        self.assertEqual(total_demand, 3.0)
+
+    def test_get_total_supply(self):
+        """Test to get the total supply of the network."""
+        # Act
+        total_supply = self.controller.get_total_supply()
+        # Assert
+        self.assertEqual(total_supply, 3.0)
+
+    def test_get_total_supply_priority(self):
+        """Test to get the total supply of the network for a certain priority."""
+        # Act
+        total_supply = self.controller.get_total_supply_priority(1)
+        # Assert
+        self.assertEqual(total_supply, 1.0)
+
+    def test__set_producers_to_max(self):
+        """Test to set the producers to the max power."""
+        # Act
+        producers = self.controller._set_producers_to_max()
+        # Assert
+        self.assertEqual(producers[self.producer1.id][PROPERTY_HEAT_DEMAND], 1.0)
+        self.assertEqual(producers[self.producer1.id][PROPERTY_TEMPERATURE_RETURN], 20.0)
+        self.assertEqual(producers[self.producer1.id][PROPERTY_TEMPERATURE_SUPPLY], 30.0)
+        self.assertTrue(producers[self.producer1.id][PROPERTY_SET_PRESSURE])
+
+        self.assertEqual(producers[self.producer2.id][PROPERTY_HEAT_DEMAND], 2.0)
+        self.assertEqual(producers[self.producer2.id][PROPERTY_TEMPERATURE_RETURN], 40.0)
+        self.assertEqual(producers[self.producer2.id][PROPERTY_TEMPERATURE_SUPPLY], 50.0)
+        self.assertFalse(producers[self.producer2.id][PROPERTY_SET_PRESSURE])
+
+    def test__set_consumer_capped(self):
+        """Test to set the consumer to the capped power."""
+        self.controller.producers[1].power = 0.5
+        # Act
+        consumers = self.controller._set_consumer_capped(datetime.now())
+        # Assert
+        self.assertEqual(consumers[self.consumer1.id][PROPERTY_HEAT_DEMAND], 1.0 * 1.5 / 3.0)
+        self.assertEqual(consumers[self.consumer2.id][PROPERTY_HEAT_DEMAND], 2.0 * 1.5 / 3.0)
+
+    def test__set_consumer_to_demand(self):
+        """Test to set the consumer to the demand."""
+        # Act
+        consumers = self.controller._set_consumer_to_demand(datetime.now())
+        # Assert
+        self.assertEqual(consumers[self.consumer1.id][PROPERTY_HEAT_DEMAND], 1.0)
+        self.assertEqual(consumers[self.consumer1.id][PROPERTY_TEMPERATURE_RETURN], 20.0)
+        self.assertEqual(consumers[self.consumer1.id][PROPERTY_TEMPERATURE_SUPPLY], 30.0)
+
+        self.assertEqual(consumers[self.consumer2.id][PROPERTY_HEAT_DEMAND], 2.0)
+        self.assertEqual(consumers[self.consumer2.id][PROPERTY_TEMPERATURE_RETURN], 40.0)
+        self.assertEqual(consumers[self.consumer2.id][PROPERTY_TEMPERATURE_SUPPLY], 50.0)
+
+    def test__set_producers_based_on_priority(self):
+        """Test to set the producers based on priority."""
+        self.controller.consumers[0].get_heat_demand.return_value = 0.5
+        # Act
+        producers = self.controller._set_producers_based_on_priority(datetime.now())
+        # Assert
+        self.assertEqual(producers[self.producer1.id][PROPERTY_HEAT_DEMAND], 1.0)
+        self.assertEqual(producers[self.producer2.id][PROPERTY_HEAT_DEMAND], 1.5)
+
+    def test_run_time_step(self):
+        """Test to run the time step."""
+        # Act
+        result = self.controller.run_time_step(datetime.now())
+
+        # Assert
+        self.assertIn(self.producer1.id, result)
+        self.assertIn(self.producer2.id, result)
+        self.assertIn(self.consumer1.id, result)
+        self.assertIn(self.consumer2.id, result)
