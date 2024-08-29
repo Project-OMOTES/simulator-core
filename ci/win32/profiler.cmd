a7dd009193e58cb613fec634a574c8a5b360e91d
samvanderzwan <sam.vanderzwan@deltares.nl>
2024-08-29 12:59:59 +0200

    removed old routine

--- ci/win32/profiler.cmd
+++
@@ -1,7 +0,0 @@
-pushd .
-cd /D "%~dp0"
-cd ..\..\
-call .\venv\Scripts\activate
-python .\src\simulator_core\infrastructure\profile_test.py
-snakeviz .\src\simulator_core\infrastructure\results.prof
-popd 
