Bug fix in getting marginal costs, Fixed setting pressure for two producers to set pressure, fixed out of bounds when flow becomes zero. COrrect minus sign for poduction asset

src/omotes_simulator_core/solver/network/assets/production_asset.py
@@ -149,7 +149,7 @@ class ProductionAsset(FallType):
                 ]
             )
             equation_object.coefficients = np.array([-1.0 + 2 * connection_point])
-            equation_object.rhs = self.mass_flow_rate_set_point
+            equation_object.rhs = -self.mass_flow_rate_set_point
         else:
             equation_object.indices = np.array(
                 [