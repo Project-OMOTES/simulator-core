4f3a415b8be5c7a663cc450b45e261625ba074ff
Ryvo Octaviano <ryvo.octaviano@tno.nl>
2024-12-09 16:44:53 +0100

    add ideal heat buffer tracking fill level

---
+++ src/omotes_simulator_core/entities/assets/heat_buffer.py
@@ -0,0 +1,163 @@
+#  Copyright (c) 2023. Deltares & TNO
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""atesCluster class."""
+from typing import Dict
+
+from omotes_simulator_core.entities.assets.asset_abstract import AssetAbstract
+from omotes_simulator_core.entities.assets.asset_defaults import (
+    DEFAULT_TEMPERATURE,
+    DEFAULT_TEMPERATURE_DIFFERENCE,
+    PROPERTY_HEAT_DEMAND,
+    PROPERTY_MASSFLOW,
+    PROPERTY_PRESSURE_RETURN,
+    PROPERTY_PRESSURE_SUPPLY,
+    PROPERTY_TEMPERATURE_RETURN,
+    PROPERTY_TEMPERATURE_SUPPLY,
+    PROPERTY_FILL_LEVEL,
+)
+
+from omotes_simulator_core.entities.assets.esdl_asset_object import EsdlAssetObject
+from omotes_simulator_core.solver.network.assets.production_asset import ProductionAsset
+from omotes_simulator_core.entities.assets.utils import (
+    heat_demand_and_temperature_to_mass_flow,
+)
+from omotes_simulator_core.solver.utils.fluid_properties import fluid_props
+
+
+class HeatBuffer(AssetAbstract):
+    """A HeatBuffer represents an asset that consumes heat and produces heat."""
+
+    temperature_supply: float
+    """The supply temperature of the asset [K]."""
+
+    temperature_return: float
+    """The return temperature of the asset [K]."""
+
+    thermal_power_allocation: float
+    """The thermal for injection (positive) or production (negative) by the asset [W]."""
+
+    mass_flowrate: float
+    """The flow rate going in or out by the asset [kg/s]."""
+
+    maximum_volume: float
+    """The maximum volume of the heat storage [m3]."""
+
+    fill_level: float
+    """The current fill level of the heat storage [%]."""
+
+    def __init__(self, asset_name: str, asset_id: str, port_ids: list[str]):
+        """Initialize a HeatBuffer object.
+
+        :param str asset_name: The name of the asset.
+        :param str asset_id: The unique identifier of the asset.
+        """
+        super().__init__(asset_name=asset_name, asset_id=asset_id, connected_ports=port_ids)
+        self.temperature_supply = DEFAULT_TEMPERATURE
+        self.temperature_return = DEFAULT_TEMPERATURE - DEFAULT_TEMPERATURE_DIFFERENCE
+        self.thermal_power_allocation = 0  # Watt
+        self.mass_flowrate = 0  # kg/s
+        self.maximum_volume = 1  # 1000 L
+        self.fill_level = 0.5  # 50 %
+        self.timestep = 3600  # seconds
+        self.solver_asset = ProductionAsset(name=self.name, _id=self.asset_id)
+
+        # Output list
+        self.output: list = []
+
+    def set_setpoints(self, setpoints: Dict) -> None:
+        """Placeholder to set the setpoints of an asset prior to a simulation.
+
+        :param Dict setpoints: The setpoints that should be set for the asset.
+        The keys of the dictionary are the names of the setpoints and the values are the values
+        """
+        # Default keys required
+        necessary_setpoints = {
+            PROPERTY_TEMPERATURE_SUPPLY,
+            PROPERTY_TEMPERATURE_RETURN,
+            PROPERTY_HEAT_DEMAND,
+        }
+        # Dict to set
+        setpoints_set = set(setpoints.keys())
+        # Check if all setpoints are in the setpoints
+        if necessary_setpoints.issubset(setpoints_set):
+            self.thermal_power_allocation = setpoints[PROPERTY_HEAT_DEMAND]
+            self.temperature_return = setpoints[PROPERTY_TEMPERATURE_RETURN]
+            self.temperature_supply = setpoints[PROPERTY_TEMPERATURE_SUPPLY]
+
+            self._calculate_massflowrate()
+            self._calculate_fill_level()
+            self._set_solver_asset_setpoint()
+        else:
+            # Print missing setpoints
+            raise ValueError(
+                f"The setpoints {necessary_setpoints.difference(setpoints_set)} are missing."
+            )
+
+    def _calculate_massflowrate(self) -> None:
+        """Calculate mass flowrate of the asset."""
+        self.mass_flowrate = heat_demand_and_temperature_to_mass_flow(
+            self.thermal_power_allocation, self.temperature_supply, self.temperature_return
+        )
+
+    def _calculate_fill_level(self) -> None:
+        """Calculate fill level of the storage."""
+        density = fluid_props.get_density(self.temperature_supply)
+        original_fill_level = self.fill_level
+        new_fill_level = (self.mass_flowrate / density * self.timestep + original_fill_level
+                          * self.maximum_volume) / self.maximum_volume
+        if new_fill_level >= 0 and new_fill_level <= 1:
+            self.fill_level = new_fill_level
+        else:
+            raise ValueError(
+                f"The new fill level is {new_fill_level}. It should be between 0 and 1."
+            )
+
+    def _set_solver_asset_setpoint(self) -> None:
+        """Set the setpoint of solver asset."""
+        if self.mass_flowrate > 0:
+            self.solver_asset.supply_temperature = self.temperature_return
+        else:
+            self.solver_asset.supply_temperature = self.temperature_supply
+        self.solver_asset.mass_flow_rate_set_point = self.mass_flowrate  # type: ignore
+
+    def add_physical_data(self, esdl_asset: EsdlAssetObject) -> None:
+        """Method to add physical data to the asset.
+
+        :param EsdlAssetObject esdl_asset: The esdl asset object to add the physical data from.
+         :return:
+        """
+        self.maximum_volume, _ = esdl_asset.get_property(
+            esdl_property_name="volume", default_value=self.maximum_volume
+        )
+        self.fill_level, _ = esdl_asset.get_property(
+            esdl_property_name="fillLevel", default_value=self.fill_level
+        )
+
+    def write_to_output(self) -> None:
+        """Placeholder to write the asset to the output.
+
+        The output list is a list of dictionaries, where each dictionary
+        represents the output of its asset for a specific timestep.
+        """
+        output_dict = {
+            PROPERTY_MASSFLOW: self.solver_asset.get_mass_flow_rate(1),
+            PROPERTY_PRESSURE_SUPPLY: self.solver_asset.get_pressure(0),
+            PROPERTY_PRESSURE_RETURN: self.solver_asset.get_pressure(1),
+            PROPERTY_TEMPERATURE_SUPPLY: self.solver_asset.get_temperature(0),
+            PROPERTY_TEMPERATURE_RETURN: self.solver_asset.get_temperature(1),
+            PROPERTY_FILL_LEVEL: self.fill_level,
+        }
+        self.output.append(output_dict)
---
+++ unit_test/entities/test_heat_buffer.py
@@ -0,0 +1,78 @@
+#  Copyright (c) 2023. Deltares & TNO
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Test Ates Cluster entities."""
+import faulthandler
+import unittest
+
+faulthandler.disable()
+from omotes_simulator_core.entities.assets.heat_buffer import HeatBuffer  # noqa: E402
+
+faulthandler.enable()
+from omotes_simulator_core.entities.assets.asset_defaults import (  # noqa: E402
+    PROPERTY_HEAT_DEMAND,
+    PROPERTY_TEMPERATURE_RETURN,
+    PROPERTY_TEMPERATURE_SUPPLY,
+)
+
+
+class HeatBufferTest(unittest.TestCase):
+    """Testcase for HeatBuffer class."""
+
+    def setUp(self) -> None:
+        """Set up before each test case."""
+        # Create a production cluster object
+        self.heat_buffer = HeatBuffer(
+            asset_name="heat_buffer", asset_id="heat_buffer_id", port_ids=["test1", "test2"]
+        )
+        faulthandler.disable()
+
+    def tearDown(self):
+        """Clean up after each test case."""
+        faulthandler.enable()
+
+    def test_injection(self) -> None:
+        """Test injection to Heat Buffer."""
+        # Arrange
+        setpoints = {
+            PROPERTY_HEAT_DEMAND: 1e4,
+            PROPERTY_TEMPERATURE_SUPPLY: 353.15,
+            PROPERTY_TEMPERATURE_RETURN: 313.15,
+        }
+
+        # Act
+        self.heat_buffer.set_setpoints(setpoints=setpoints)
+
+        # Assert
+        self.assertAlmostEqual(self.heat_buffer.temperature_supply, 353.15, delta=0.1)
+        self.assertAlmostEqual(self.heat_buffer.temperature_return, 313.155, delta=0.1)
+        self.assertAlmostEqual(self.heat_buffer.fill_level, 0.72, delta=0.01)
+
+    def test_production(self) -> None:
+        """Test production from Heat Buffer."""
+        # Arrange
+        setpoints = {
+            PROPERTY_HEAT_DEMAND: -1e4,
+            PROPERTY_TEMPERATURE_SUPPLY: 353.15,
+            PROPERTY_TEMPERATURE_RETURN: 313.15,
+        }
+
+        # Act
+        self.heat_buffer.set_setpoints(setpoints=setpoints)
+
+        # Assert
+        self.assertAlmostEqual(self.heat_buffer.temperature_supply, 353.15, delta=0.1)
+        self.assertAlmostEqual(self.heat_buffer.temperature_return, 313.15, delta=0.1)
+        self.assertAlmostEqual(self.heat_buffer.fill_level, 0.27, delta=0.01)
