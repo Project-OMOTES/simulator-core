2ab39f92d84310d140146ffde6d5d38a81b2a2c9
Sam van der Zwan <sam.vanderzwan@deltares.nl>
2025-03-20 14:55:21 +0100

    Refactored to use standard graph libary

--- src/omotes_simulator_core/adapter/utility/graph.py
+++ src/omotes_simulator_core/adapter/utility/graph.py
@@ -13,19 +13,19 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """Module of simple graph class to help detect connectivity of the network."""
+import networkx as nx
 
 
 class Graph:
     """Main class for the graph."""
 
-    def __init__(self) -> None:
+    def __init__(self, weight=0) -> None:
         """Constructor of the class.
 
         The graph is stored as a dictionary of nodes and a list of edges.
         Both are initialised empty.
         """
-        self.nodes: dict[str, Node] = {}
-        self.edges: list[Edge] = []
+        self.graph = nx.Graph()
 
     def add_node(self, node: str) -> None:
         """Method to add a node to the graph.
@@ -33,9 +33,9 @@ class Graph:
         When the node already exists in the graph, an error is raised.
         :param str node: Name of the node to add.
         """
-        if node in self.nodes:
+        if self.node_exists(node):
             raise ValueError(f"Node {node} already exists in the graph.")
-        self.nodes[node] = Node(node)
+        self.graph.add_node(node)
 
     def connect(self, node1: str, node2: str) -> None:
         """Method to connect two nodes in the graph.
@@ -53,16 +53,14 @@ class Graph:
             raise ValueError(f"Node {node2} does not exist in the graph.")
         if self.is_directly_connected(node1, node2):
             return
-        self.edges.append(Edge(node1, node2))
-        self.nodes[node1].add_edge(self.edges[-1])
-        self.nodes[node2].add_edge(self.edges[-1])
+        self.graph.add_edge(node1, node2)
 
     def node_exists(self, node: str) -> bool:
         """Method to check if a node exists in the graph.
 
         :param str node: Name of the node to check.
         """
-        return node in self.nodes
+        return self.graph.has_node(node)
 
     def is_directly_connected(self, node1: str, node2: str) -> bool:
         """Method to check if two nodes are connected in the graph.
@@ -77,10 +75,8 @@ class Graph:
             raise ValueError(f"Node {node1} does not exist in the graph.")
         if not self.node_exists(node2):
             raise ValueError(f"Node {node2} does not exist in the graph.")
-        for edge in self.nodes[node1].edges:
-            # this looks a bit weir, but we need to also check the other edges, so we cannot simply
-            # return the value of the first edge which is checked.
-            if edge.connected(node2):
+        for node in self.graph.neighbors(node1):
+            if node == node2:
                 return True
         return False
 
@@ -98,65 +94,14 @@ class Graph:
             raise ValueError(f"Node {node1} does not exist in the graph.")
         if not self.node_exists(node2):
             raise ValueError(f"Node {node2} does not exist in the graph.")
-        node_to_check = [self.nodes[node1]]
-        nodes_checked = []
-        while node_to_check:
-            node = node_to_check.pop(0)
-            nodes_checked.append(node)
-            for edge in node.edges:
-                for node_name in edge.nodes:
-                    node = self.nodes[node_name]
-                    if node == self.nodes[node2]:
-                        return True
-                    if node not in nodes_checked and node not in node_to_check:
-                        node_to_check.append(node)
-        return False
+        try:
+            nx.shortest_path(self.graph, node1, node2)
+            return True
+        except nx.NetworkXNoPath:
+            return False
 
-
-class Edge:
-    """Class for the edges in the graph."""
-
-    def __init__(self, node1: str, node2: str) -> None:
-        """Constructor of the class.
-
-        An edge can only be connected to two nodes. The nodes are stored in a list.
-        Node1 and node2 need to be different nodes, otherwise an error is raised.
-
-        :param Node node1: First node connected to the edge.
-        :param Node node2: Second node connected to the edge.
-        """
-        if node1 == node2:
-            raise ValueError("An edge cannot connect a node to itself.")
-        self.nodes = [node1, node2]  # list of nodes connected to the edge
-
-    def connected(self, node_name: str) -> bool:
-        """Method to check if a node is connected to the edge.
-
-        If the node is connected to the edge, the method returns True, otherwise False.
-        :param str node: Name of the node to check.
-        """
-        return node_name in self.nodes
-
-
-class Node:
-    """Class for the nodes in the graph."""
-
-    def __init__(self, node: str) -> None:
-        """Constructor of the class.
-
-        :param str node: Name of the node.
-        """
-        self.node_name = node
-        self.edges: list[Edge] = []  # list of edges connected to the node
-
-    def add_edge(self, edge: Edge) -> None:
-        """Method to add an edge to the node.
-
-        There is no limit to amount fo edges connected to one node.
-        The edge is only added when it is not already in the list.
-        No error is given when the edge is already in the list.
-
-        :param Edge edge: Edge to add to the node.
-        """
-        if edge not in self.edges:
-            self.edges.append(edge)
+    def get_path(self, node1: str, node2: str) -> list[str]:
+        try:
+            return nx.shortest_path(self.graph, node1, node2)
+        except nx.NetworkXNoPath:
+            return []
