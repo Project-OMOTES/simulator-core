f49823d7b301f5aac27cf6843cffdb7bdef2cab3
Ashraf Almohagry (Deltares) <ashraf.al-mohagry@deltares.nl>
2025-08-01 11:33:59 +0200

    fixed formatting.

--- unit_test/entities/test_esdl_object.py
+++ unit_test/entities/test_esdl_object.py
@@ -16,9 +16,9 @@
 """Test esdl object class."""
 import unittest
 from pathlib import Path
-from pandas.testing import assert_frame_equal
 
 import esdl
+from pandas.testing import assert_frame_equal
 
 from omotes_simulator_core.adapter.transforms.esdl_asset_mapper import EsdlAssetMapper
 from omotes_simulator_core.adapter.transforms.string_to_esdl import (
@@ -28,8 +28,8 @@ from omotes_simulator_core.entities.assets.demand_cluster import DemandCluster
 from omotes_simulator_core.entities.assets.pipe import Pipe
 from omotes_simulator_core.entities.assets.production_cluster import ProductionCluster
 from omotes_simulator_core.entities.esdl_object import EsdlObject
-from omotes_simulator_core.infrastructure.utils import pyesdl_from_file
 from omotes_simulator_core.entities.utility.influxdb_reader import get_data_from_profile
+from omotes_simulator_core.infrastructure.utils import pyesdl_from_file
 
 
 class EsdlObjectTest(unittest.TestCase):
